9b6841437ec726047abf4e75e5346ef903763f15
Mohit <mohitbite@gmail.com>
2024-04-02 10:07:43 +0530

    object structure enhanced

--- foodVilla/src/components/RestaurantMenu/RestaurantMenu.jsx
+++ foodVilla/src/components/RestaurantMenu/RestaurantMenu.jsx
@@ -6,19 +6,20 @@ const RestaurantMenu = () => {
   const { resId } = useParams();
 
   const resInfo = useRestaurantMenu(resId);
+  console.log(resInfo)
 
   const [showIndex, setShowIndex] = useState(null);
 
   if (resInfo === null) return <Shimmer />;
 
   const { name, cuisines, costForTwoMessage } =
-    resInfo?.cards[0]?.card?.card?.info;
+    resInfo?.cards[2]?.card?.card?.info;
 
   // const { itemCards } =
   //   resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
   const categories =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (c) =>
         c.card?.["card"]?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
